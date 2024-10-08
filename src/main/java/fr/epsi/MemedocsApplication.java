ebd90128a5507c3144600a1c3c841736b0e15d2e
Gurvann Brenne <gurvann.brenne@gmail.com>
2024-10-08 12:10:41 +0200

    :tada: : initial commit

---
+++ src/main/java/fr/epsi/MemedocsApplication.java
@@ -0,0 +1,13 @@
+package fr.epsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemedocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemedocsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/epsi/MemedocsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.epsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemedocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
